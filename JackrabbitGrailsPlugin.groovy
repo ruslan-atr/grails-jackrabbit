5cc74e7d1b3fdbbc804745374aaca1c0ff6113a9
Sergey Nebolsin <nebolsin@gmail.com>
2008-05-21 12:20:57 +0000

    Initial plugin's import.

---
+++ JackrabbitGrailsPlugin.groovy
@@ -0,0 +1,51 @@
+/* Copyright 2004-2005 Graeme Rocher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * A plugin for the Grails framework (http://grails.org) that provides JackRabbit
+ * as an implementation layer for the Grails JCR plugin.
+ *
+ * @author Sergey Nebolsin
+ */
+class JackrabbitGrailsPlugin {
+    def version = 0.1
+    def dependsOn = [:]
+	
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
+   
+    def doWithApplicationContext = { applicationContext ->
+        // TODO Implement post initialization spring config (optional)		
+    }
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional)
+    }
+	                                      
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
+	
+    def onChange = { event ->
+        // TODO Implement code that is executed when this class plugin class is changed  
+        // the event contains: event.application and event.applicationContext objects
+    }
+                                                                                  
+    def onApplicationChange = { event ->
+        // TODO Implement code that is executed when any class in a GrailsApplication changes
+        // the event contain: event.source, event.application and event.applicationContext objects
+    }
+}
